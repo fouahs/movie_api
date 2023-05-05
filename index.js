85328bafbb25160ec3894553e24a5462e613ea92
fouad <f.ahsayni@pm.me>
2023-05-05 17:01:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const { check, validationResult } = require("express-validator");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect("mongodb://127.0.0.1:27017/myFlixDB", { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect("mongodb://127.0.0.1:27017/myFlixDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect("process.env.CONNECTION_URI", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
