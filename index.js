16efd98d4e6439ca5edc7cb97f398e2245e36cdf
fouad <f.ahsayni@pm.me>
2023-05-05 17:13:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Users = Models.User;
 
 // mongoose.connect("mongodb://127.0.0.1:27017/myFlixDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect("process.env.CONNECTION_URI", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
